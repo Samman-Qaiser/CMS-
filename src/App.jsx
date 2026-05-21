8476d2db3e18e90b937b06bd7dcdac0052700c12
Samman Qaiser <sammansheikh6@gmail.com>
2026-05-21 10:50:54 +0500

    Public route and setcredential slice created and updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,59 @@ import SignUp from './auth/SignUp'
 import ForgotPassword from './auth/ForgotPassword'
 import DashboardRoutes from './features/Dashboard/routes/DashboardRoutes'
 import ResetPassword from "./auth/ResetPassword";
+import PublicRoute from "./route/PublicRoute";
 
 
 const App = () => {
-return (
-  <ThemeProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard/*" element={<DashboardRoutes />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-      </Routes>
-      {/* ← DashboardRoutes duplicate hata diya */}
-    </BrowserRouter>
-  </ThemeProvider>
-);
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+
+          {/* Protected Dashboard */}
+          <Route
+            path="/dashboard/*"
+            element={
+           
+                <DashboardRoutes />
+        
+            }
+          />
+
+          {/* Public Routes */}
+          <Route
+            path="/"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+
+          <Route
+            path="/signup"
+            element={
+              <PublicRoute>
+                <SignUp />
+              </PublicRoute>
+            }
+          />
+
+          <Route
+            path="/forgot-password"
+            element={
+              <PublicRoute>
+                <ForgotPassword />
+              </PublicRoute>
+            }
+          />
+
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 };
 
 export default App
